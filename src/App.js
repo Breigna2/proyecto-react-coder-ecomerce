7c8829fc29d83df8f983d45efa1881f5aa92af74
Victor G <96495544+Breigna2@users.noreply.github.com>
2022-12-10 22:27:45 -0600

    Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import NavBar from "./components/navBar/NavBar";
 import ItemListContainer from "./components/itemListContainer/ItemListContainer";
+import CustomButton from "./components/customButton/CustomButton";
+import Counter from "./components/counter/Counter";
 
 function App() {
   return (
     <div>
       <NavBar/>
+
+      <Counter/>
+
       <ItemListContainer greeting={"Buenas tardes, espero todo vaya de maravilla :D"}/>
+      
+      <CustomButton texto={"login"}/>
     </div>
   );
 }
