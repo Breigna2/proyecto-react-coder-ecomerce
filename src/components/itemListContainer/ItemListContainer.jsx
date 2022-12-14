8df4caed0944c25c70824461d7c4462f32952ebe
Victor G <96495544+Breigna2@users.noreply.github.com>
2022-12-13 22:17:34 -0600

    Update13

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import "./itemListContainer.css"
 
 //import CustomButton from "../customButton/CustomButton"
 
@@ -7,17 +8,18 @@ const ItemListContainer = ({greeting}) => {
 
   const [ isDark , setIsDark ] = useState(false)
 
-  const toogleMode = ()=>{
+  const tooggleMode = ()=>{
+    
     setIsDark(!isDark)
 
 }
 
   return (
-    <>
+    <div className = 'container-items'>
         <h1>{greeting}</h1>
         {/*<CustomButton texto={"sumar"}/>*/}
-        <button onClick={toogleMode}>cambiar</button>
-    </>
+        <button onClick={tooggleMode}>{isDark ? "cambiar a modo claro" : "cambiar a modo oscuro"}</button>
+    </div>
   )
 }
 
