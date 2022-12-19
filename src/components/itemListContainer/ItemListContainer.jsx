ba8b479248252ac756a312fcf814f1e2dbffa005
Victor G <96495544+Breigna2@users.noreply.github.com>
2022-12-18 21:54:45 -0600

    Main update1812

--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Counter from '../counter/Counter'
 import "./itemListContainer.css"
+import { productos } from "../../productosMock"
 
 
 //import CustomButton from "../customButton/CustomButton"
@@ -16,6 +17,8 @@ const ItemListContainer = ({greeting}) => {
 
 }
 
+const [items, setItems] = useState([])
+
 useEffect( ()=>{
 
   console.log("PRIMER EFECTO") 
@@ -26,12 +29,51 @@ const onAdd = ()=>{
   console.log("Vamos bien");
 }
 
+useEffect(()=>{
+
+const task = new Promise ((resolve, reject)=>{
+  setTimeout(() =>{
+    resolve(productos)
+}, 5000);
+
+})
+
+
+
+task
+.then( (res)=>{ setItems( res )})
+.catch((error)=>{"Se rechazó"})
+
+}, [] )
+
+console.log(items)
+
+//console.log("Esta es la promesa", task)
+
   return (
     <div className = {isDark ? "container-items-dark" : "container-items-light"} >
         <h1>{greeting}</h1>
         {/*<CustomButton texto={"sumar"}/>*/}
         <button onClick={tooggleMode}>{isDark ? "cambiar a modo claro" : "cambiar a modo oscuro"}</button>
         <Counter stock={5} initial={1} onAdd={onAdd}/>
+
+        {
+          items.map((element)=>{
+            return (<div key={element.name}>
+              <h2>
+               {element.name} 
+              </h2>
+              <h3>
+              {element.price}
+              </h3>
+              <h3>
+              {element.description}
+              </h3>
+              </div>
+            );
+          })
+        }
+
     </div>
   )
 }
---
+++ src/productosMock.js
@@ -0,0 +1,50 @@
+export const productos = [
+    {
+       name: "Fender Jazz-Bass",
+       price: 18500,
+       description: "Un clásico multitono que te recuerda los grandes del Jazz",
+       category: "importados",
+       stock: 7,
+       img: "https://res.cloudinary.com/dldvgt3pe/image/upload/v1666764083/guitare-basse-fender-jazz-bass-70-classic-3ts_qitth7.png"
+    },
+    {
+       name: "Fender Precision-Bass",
+       price: 15500,
+       description: "El instrumento clásico del rock",
+       category: "nacionales",
+       stock: 7,
+       img:"https://res.cloudinary.com/dldvgt3pe/image/upload/v1664153970/fender-precision-bass-mexican-51957904_wfxcg9.jpg"
+    },
+    {
+       name: "Jackson standard",
+       price: 14300,
+       description: "El rugido del metal en 4 cuerdas",
+       category: "importados",
+       stock: 5,
+       img:"https://res.cloudinary.com/dldvgt3pe/image/upload/v1666669954/%C3%ADndice_mladtg.jpg"
+    },
+    {
+       name: "Musicman Stringray",
+       price: 35000,
+       description: "La voz ruda de la música",
+       category: "importados",
+       stock: 6,
+       img:"https://res.cloudinary.com/dldvgt3pe/image/upload/v1666670339/62358_xzch9e.png"
+    },
+    {
+       name: "Rickenbacker 4003",
+       price: 47500,
+       description: "La etiqueta y clase en un instrumento",
+       category: "importados",
+       stock: 3,
+       img:"https://res.cloudinary.com/dldvgt3pe/image/upload/v1664154170/Factory-Custom-Milk-White-4-Strings-Rickenbacker-4003-Electric-Bass-Guitar-Rosewood-Fretboard_zphve5.jpg"
+    },
+    {
+       name: "Ibanez 3005",
+       price: 11000,
+       description: "Amplia gama de sonidos para todo músico",
+       category: "nacionales",
+       stock: 2,
+       img:"https://res.cloudinary.com/dldvgt3pe/image/upload/v1664157593/images_wfnn9q.jpg"
+    }
+]
